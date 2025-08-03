f7561d2317f98bcb8b003b01b1eaa50bb80c8971
Parazeya <zezarUS@bk.ru>
2025-08-03 04:32:12 +0300

    Swagger docs

--- fix_imports.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Исправляет импорты GizmoClient.js во всех категориях v1 и v2
-find ./src/v1 -name '*.js' -exec sed -i 's|from ["\']GizmoClient\.js["\']|from "../GizmoClient.js"|g' {} +
-find ./src/v2 -name '*.js' -exec sed -i 's|from ["\']GizmoClient\.js["\']|from "../GizmoClient.js"|g' {} +
